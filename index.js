316ead53abc238afd1d63db08a6e7fc617291d60
preslavaDoceo <preslava@doceosoftware.com>
2025-02-07 13:49:16 +0100

    prova 2

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 const t = TelloPowerUp.iframe();
 
-// function onButtonClick(t, options){
-//     const cardId = options.card.id;
-
-//     t.card('all').then(function(cardData){
-//         console.log(cardData);
-//         sendToServer(cardData);
-//     });
-// }
-
-
-// t.render(() => {
-//     const btn = document.createElement('button');
-//     btn.textContent = 'Share Information';
-//     document.body.appendChild(btn);
-
-//     btn.addEventListener( 'click', async () => {
-//         const cardId = await t.card('id');
-//         const cardName = await t.card('name');
-//         const cardDescription = await t.card('desc');
-//         const cardLabels = await t.card('labls')
-
-//         const cardData = {
-//             id:cardId,
-//             name:cardName,
-//             description:cardDescription,
-//             labels:cardLabels
-//         };
-
-//         await sendToServer(cardData);
-//     });
-// });
-
 const cardButton = (t, options) =>{
     return [
         {
